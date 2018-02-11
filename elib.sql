92676afca3bdb7412ca242cf215a09af34f9033a
Justro <thepiesaresquared@gmail.com>
2018-02-11 14:55:18 +0800

    added transactions table

--- elib.sql
+++ elib.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2018 at 07:03 AM
+-- Generation Time: Feb 11, 2018 at 07:54 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -51,6 +51,20 @@ CREATE TABLE `copy` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `copy` text NOT NULL,
+  `user` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `date` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -60,6 +74,14 @@ CREATE TABLE `user` (
   `name` text NOT NULL,
   `batch` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `id_code`, `name`, `batch`) VALUES
+(1, '', 'Justine Romero', '12'),
+(2, '', 'fewhgre', 'shr');
 
 --
 -- Indexes for dumped tables
@@ -75,6 +97,12 @@ ALTER TABLE `book`
 -- Indexes for table `copy`
 --
 ALTER TABLE `copy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -98,10 +126,15 @@ ALTER TABLE `book`
 ALTER TABLE `copy`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
