14eba28fca5afd1a827c8c3efb3f806537bceafd
Jadsen <mjbailonpuerto@gmail.com>
2018-02-21 12:01:16 +0800

    edits in book page

--- FrontEnd/pages/books.php
+++ FrontEnd/pages/books.php
@@ -263,7 +263,7 @@
                                         <td>
                                           <div>
                                             <a>
-                                              <button class="btn btn-primary btn-xs">
+                                              <button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#modalborrow">
                                                 Borrow
                                               </button>
                                             </a>
@@ -274,7 +274,29 @@
                                   </table>
                                 </div>
                                 <div class="modal-footer">
-                                  
+                                  <button type="submit" class="btn btn-default btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="modal fade" id="modalborrow" role="dialog">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                  <h4 style="color:red;"><span class="glyphicon glyphicon-book"></span>Borrow book</h4>
+                                </div>
+                                <div class="modal-body">
+                                  <form>
+                                      <div class="form-group">
+                                        <label for="callnumber">Borrower:</label>
+                                        <input type="text" class="form-control" id="cn" placeholder="Enter Student ID">
+                                      </div>
+                                      <button type="submit" class="btn btn-success btn-block"> Submit</button> 
+                                  </form>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="submit" class="btn btn-default btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
                                 </div>
                               </div>
                             </div>
