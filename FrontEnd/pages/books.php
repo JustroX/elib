56d8bb314db4436374d42519a8c6ce825af1e74f
Jadsen <mjbailonpuerto@gmail.com>
2018-03-26 19:09:06 +0800

    Added add copy button and modal

--- FrontEnd/pages/books.php
+++ FrontEnd/pages/books.php
@@ -257,6 +257,7 @@
                                         <h5>20</h5>
                                       </div> 
                                   </form>
+                                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addcopymodal">Add copy/ies</button>
                                   <table width="100%" class="table table-striped table-bordered table-hover table-responsive" id="dataTables-example">
                                     <thead>
                                       <tr>
@@ -315,6 +316,38 @@
                                 </div>
                               </div>
                             </div>
+                          </div>
+                          <div class="modal fade" id="addcopymodal" role="dialog">
+                            <div class="modal-dialog">
+
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 style="color:red;"><span class="glyphicon glyphicon-book"></span> Add another copy</h4>
+                                  </div>
+                                  <div class="modal-body">
+                                    <form>
+                                      <div class="form-group">
+                                        <label for="access">Access no:</label>
+                                        <input type="text" class="form-control" id="an" placeholder="Enter Access no">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="date">Date:</label>
+                                        <input type="text" class="form-control" id="da" placeholder="Enter Date">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="nocopies">No. of copies:</label>
+                                        <input type="text" class="form-control" id="nc" placeholder="Enter No of copies">
+                                      </div>
+                                      <button type="submit" class="btn btn-success btn-block"> Submit</button>
+                                    </form>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="submit" class="btn btn-default btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                                  </div>
+                                </div>
+                              </div>
                           </div>
                           <div class="modal fade" id="modal2" role="dialog">
                             <div class="modal-dialog">
